e9d3071fb8868234df12f2b313ab11fbeb22fdca
Luke Parker <moongateuk@gmail.com>
2016-10-03 00:07:38 +0100

    Started project

---
+++ gbjam/GBJam-5/gbjam-5-project/Assets/Scripts/Display/GBRender.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+[AddComponentMenu("Image Effects/Pixel GB")]
+public class GBRender : MonoBehaviour {
+
+	public int h = 166;
+	public int w = 144;
+	protected void Start()
+	{
+		if (!SystemInfo.supportsImageEffects)
+		{
+			enabled = false;
+			return;
+		}
+	}
+	void Update()
+	{
+
+
+
+	}
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		source.filterMode = FilterMode.Point;
+		RenderTexture buffer = RenderTexture.GetTemporary(w, h, -1);
+		buffer.filterMode = FilterMode.Point;
+		Graphics.Blit(source, buffer);
+		Graphics.Blit(buffer, destination);
+		RenderTexture.ReleaseTemporary(buffer);
+	}
+}
